81cf3da85af69ec8a3f6ee6d3ad7393aecb31f24
air <xu.jm01@gmail.com>
2014-06-24 23:39:16 +0800

    合并分支

---
+++ AlphaClient/AlphaCommon/CAlphaWindow.cpp
@@ -0,0 +1,84 @@
+#include "stdafx.h"
+#include <windows.h>
+#include "CAlphaWindow.h"
+
+LRESULT CALLBACK SWindowContext::WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+VOID CALLBACK SWindowContext::TimeProp(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+{
+
+}
+
+
+CAlphaWindow::CAlphaWindow()
+{
+	
+}
+
+
+CAlphaWindow::~CAlphaWindow()
+{
+
+}
+
+
+void CAlphaWindow::Initialize( void* pContext, uint16 width, uint16 height, char *szTitle, uint16 iconID)
+{
+	WNDCLASSEX wndclsex;
+	wndclsex.cbSize = sizeof(WNDCLASSEX);
+	wndclsex.cbClsExtra = 0;
+	wndclsex.cbWndExtra = 0;
+	wndclsex.hbrBackground = NULL;
+	wndclsex.hIcon = NULL;
+	wndclsex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclsex.hInstance = HINSTANCE(pContext);
+	wndclsex.lpfnWndProc = SWindowContext::WndProc;
+	wndclsex.lpszClassName = "CAlphaWindow";
+	wndclsex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	wndclsex.lpszMenuName = NULL;
+	wndclsex.hIconSm = LoadIcon(wndclsex.hInstance, MAKEINTRESOURCE(iconID));
+
+	RegisterClassEx(&wndclsex);
+
+	HWND hWnd;
+	hWnd = CreateWindowEx(WS_EX_APPWINDOW, "CAlphaWindow", szTitle, WS_OVERLAPPEDWINDOW, 0, 0, width, height, NULL, NULL, wndclsex.hInstance, this);
+
+	m_Context.m_pContext = wndclsex.hInstance;
+	m_Context.m_pHandle = hWnd;
+
+	int32 cx = GetSystemMetrics(SM_CXSCREEN);
+	int32 cy = GetSystemMetrics(SM_CYSCREEN);
+
+	int32 posX = (cx - width) / 2;
+	int32 posY = (cy - height) / 2;
+
+	MoveWindow((HWND)GetHandle(), posX, posY, width, height, false);
+
+	ShowWindow((HWND)GetHandle(), SW_SHOW);
+
+	UpdateWindow((HWND)GetHandle());
+
+	SetTimer((HWND)GetHandle(), uint32(this), 50, m_Context.TimeProp);
+
+	OnCreated();
+}
+
+
+
+void* CAlphaWindow::GetHandle()
+{
+	return m_Context.m_pContext;
+}
+
+void CAlphaWindow::OnCreated()
+{
+
+}
+
+void CAlphaWindow::Destroy()
+{
+
+}
---
+++ AlphaClient/AlphaTest/CAlphaTest.cpp
@@ -0,0 +1,19 @@
+// AlphaTest.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "CAlphaTest.h"
+#include "AlphaCommon/CAlphaWindow.h"
+
+CAlphaWindow window;
+
+void init()
+{
+	window.Initialize(NULL, 800, 600, "MyTestTitle", 0);
+}
+
+int main()
+{
+	init();
+	return 0;
+}
